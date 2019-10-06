2fe92c1443d626e11287ef6d454b7b221b520e79
qkreltms <you@example.com>
2019-10-06 13:04:19 +0900

    Initial commit

---
+++ Examples/BasicTutorial/AdvancedFeatures/05-shuffle/pages/todoListPage/todoList/TodoList.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import Item from './Item';
+import TextField from "./TextField";
+import runToast from "../../../components/toast"
+
+const TodoList = ({ todos, setTodos }) => {
+  const [text, setText] = useState("");
+
+  const onInputChangeHandler = text => {
+    setText(text);
+  };
+
+  const addTodoHandler = e => {
+    addTodo(text);
+    setText('');
+  };
+
+  const addTodo = text => {
+    if (!text) { return; }
+
+    setTodos([
+      ...todos,
+      { id: Math.floor(Math.random() * 10000 + 1), text: text, checked: false },
+    ]);
+  };
+
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const toggleCheckedTodo = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  };
+
+  const onRemoveButtonClickHandler = id => e => {
+    removeTodo(id);
+    runToast(`${id}: is deleted!`, "LONG", "CENTER");
+  }
+
+  const onCheckButtonClickHandler = id => e => {
+    toggleCheckedTodo(id);
+  }
+
+  return (
+    <View>
+      <TextField onSubmitEnd={addTodoHandler} value={text} onTextChange={onInputChangeHandler} />
+      <ScrollView contentContainerStyle={styles.todoListContainer}>
+        {todos.map((todo, index) => (
+          <Item
+            key={index}
+            todo={todo}
+            onRemove={onRemoveButtonClickHandler}
+            onCheck={onCheckButtonClickHandler}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  todoListContainer: {
+    backgroundColor: 'white'
+  }
+});
+
+export default TodoList;
