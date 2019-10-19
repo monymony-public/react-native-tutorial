3ac7fb8f85b05a5b6e10ef67bb529d797f902a51
qkreltms <you@example.com>
2019-10-19 14:24:34 +0900

    Rename: RainbowCreator to createRainbow

--- Examples/BasicTutorial/AdvancedFeatures/07-hoc/hoc/withRainbow.js
+++ Examples/BasicTutorial/AdvancedFeatures/07-hoc/hoc/withRainbow.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const withRainbow = WrappedComponent => {
-    const RainbowCreator = props => {
+    const createRainbow = props => {
         const myFavoriteColors = ["red", "orange", "yellow", "green", "blue", "purple"];
         const pickOneColor = () => myFavoriteColors[Math.floor(Math.random() * myFavoriteColors.length)];
 
         return <WrappedComponent {...props} color={pickOneColor()} />
     }
 
-    return RainbowCreator;
+    return createRainbow;
 }
 
 export default withRainbow; 
